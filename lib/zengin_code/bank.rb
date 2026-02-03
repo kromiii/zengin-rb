f0aea81c7f0aec56bf6480208646879939e1d33a
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2026-02-03 22:25:36 +0900

    consider race condition

--- lib/zengin_code/bank.rb
+++ lib/zengin_code/bank.rb
@@ -14,13 +14,18 @@ class ZenginCode::Bank
     end
 
     def all
-      unless @data_loaded
-        @banks ||= {}
-        json = JSON.load(File.read(ZenginCode::DATA_DIR.join('banks.json')))
-        json.values.each do |bank_data|
-          new(bank_data)
+      return @banks if @data_loaded
+
+      @banks_mutex ||= Mutex.new
+      @banks_mutex.synchronize do
+        unless @data_loaded
+          @banks ||= {}
+          json = JSON.load(File.read(ZenginCode::DATA_DIR.join('banks.json')))
+          json.values.each do |bank_data|
+            new(bank_data)
+          end
+          @data_loaded = true
         end
-        @data_loaded = true
       end
       @banks
     end
@@ -32,18 +37,23 @@ class ZenginCode::Bank
     @kana = options['kana']
     @hira = options['hira']
     @roma = options['roma']
+    @branches_mutex = Mutex.new
     self.class.send(:[]=, code, self)
   end
 
   attr_reader :code, :name, :kana, :hira, :roma
 
   def branches
-    if @branches.nil?
-      @branches = {}
-      json = JSON.load(File.read(ZenginCode::DATA_DIR.join("branches/#{code}.json")))
-      json.values.each do |branch_data|
-        branch = ZenginCode::Branch.new(self, branch_data)
-        @branches[branch.code] = branch
+    return @branches if @branches
+
+    @branches_mutex.synchronize do
+      if @branches.nil?
+        @branches = {}
+        json = JSON.load(File.read(ZenginCode::DATA_DIR.join("branches/#{code}.json")))
+        json.values.each do |branch_data|
+          branch = ZenginCode::Branch.new(self, branch_data)
+          @branches[branch.code] = branch
+        end
       end
     end
     @branches
